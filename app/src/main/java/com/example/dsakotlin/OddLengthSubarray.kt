af540ce764f7b9803ce612220f0c126b8db0823d
NikhilBajpai <nikhil.cse16@gmail.com>
2021-09-09 10:54:37 +0530

    week 0 day 4

---
+++ app/src/main/java/com/example/dsakotlin/OddLengthSubarray.kt
@@ -0,0 +1,18 @@
+package com.example.dsakotlin
+
+fun main() {
+    fun sumOddLengthSubarrays(arr: IntArray): Int {
+        val subArraySum = 0
+
+        for(i in 0..arr.size step 2){
+            println(i)
+
+        }
+
+
+        return 0
+
+    }
+    val arr  = intArrayOf(1,4,2,5,3)
+    println(sumOddLengthSubarrays(arr))
+}
