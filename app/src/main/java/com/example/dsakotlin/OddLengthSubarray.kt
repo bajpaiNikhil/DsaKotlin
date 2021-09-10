58e3db27c44545c10b21bb58c221d23ea3cc9d69
NikhilBajpai <nikhil.cse16@gmail.com>
2021-09-10 12:49:29 +0530

    week 0 day 5

--- app/src/main/java/com/example/dsakotlin/OddLengthSubarray.kt
+++ app/src/main/java/com/example/dsakotlin/OddLengthSubarray.kt
@@ -1,18 +1,25 @@
 package com.example.dsakotlin
 
 fun main() {
+
+
     fun sumOddLengthSubarrays(arr: IntArray): Int {
-        val subArraySum = 0
-
-        for(i in 0..arr.size step 2){
-            println(i)
-
+        var sum = 0
+        val n: Int = arr.size
+        for (i in 0 until n) {
+            sum += arr[i] //always add current num
+            var tempSum = arr[i]
+            for (j in i + 1 until arr.size) {
+                tempSum += arr[j] //add odd and even indexes to tempSum
+                if ((j - i + 1) % 2 == 1) { // only add when index is odd such as 1,3,5 ...
+                    sum += tempSum
+                }
+            }
         }
-
-
-        return 0
+        return sum
 
     }
+
     val arr  = intArrayOf(1,4,2,5,3)
     println(sumOddLengthSubarrays(arr))
 }
