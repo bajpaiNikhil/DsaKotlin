ac3728ddb8f28f1b9b85757162b1e91373eb520e
NikhilBajpai <nikhil.cse16@gmail.com>
2021-09-12 20:25:38 +0530

    week 0 day 7

--- app/src/main/java/com/example/dsakotlin/MatrixDiagonalSum.kt
+++ app/src/main/java/com/example/dsakotlin/MatrixDiagonalSum.kt
@@ -3,10 +3,20 @@ package com.example.dsakotlin
 fun main() {
     fun diagonalSum(mat: Array<IntArray>): Int {
 
-
-
-        return 1
-
+        val diagonalElement = mutableListOf<Int>()
+        val sizeMatrix = mat.size
+        for(i in 0 until sizeMatrix){
+            //println("${mat[i][i]} , ${mat[i][sizeMatrix-i-1]}")
+            diagonalElement.add(mat[i][i])
+            diagonalElement.add(mat[i][sizeMatrix-i-1])
+        }
+        //println(diagonalElement)
+        return if(sizeMatrix % 2 != 0){
+            diagonalElement.removeAt(sizeMatrix)
+            diagonalElement.sum()
+        }else{
+            diagonalElement.sum()
+        }
     }
 
     val matrix = arrayOf(intArrayOf(1,2,3) , intArrayOf(4,5,6) , intArrayOf(7,8,9))
