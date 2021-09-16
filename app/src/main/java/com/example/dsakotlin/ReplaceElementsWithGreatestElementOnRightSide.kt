e4d771899e6e9b9dc99d0a28d2d62bc0b8e2b8c3
NikhilBajpai <nikhil.cse16@gmail.com>
2021-09-16 20:25:23 +0530

    week 0 day 11

--- app/src/main/java/com/example/dsakotlin/ReplaceElementsWithGreatestElementOnRightSide.kt
+++ app/src/main/java/com/example/dsakotlin/ReplaceElementsWithGreatestElementOnRightSide.kt
@@ -3,9 +3,31 @@ package com.example.dsakotlin
 fun main() {
 
     fun replaceElements(arr: IntArray): IntArray {
-
         return arr
+//
+//        for(i in arr.indices){
+//            val maxIs = arr.copyOfRange(i+1 ,arr.lastIndex+1)
+//            //println(" .... ${maxIs.contentToString()} , ${arr[i]} , $i , ${arr.lastIndex}" )
+//            arr[i] = maxIs.maxOrNull() ?: -1
+//            println("${maxIs.contentToString()} , ${arr[i]} , $i , ${arr.lastIndex}" )
+//        }
+//        return arr
     }
+        fun replaceElements1(arr: IntArray): IntArray {
+            if (arr.size == 1) {
+                arr[0] = -1
+                return arr
+            }
+            var max = arr[arr.size - 1]
+            var temp = 0
+            for (i in arr.size - 2 downTo 0) {
+                temp = arr[i]
+                arr[i] = max
+                max = max.coerceAtLeast(temp)
+            }
+            arr[arr.size - 1] = -1
+            return arr
+        }
     val array = intArrayOf(17,18,5,4,6,1)
-    println(replaceElements(array))
+    println(replaceElements1(array).contentToString())
 }
