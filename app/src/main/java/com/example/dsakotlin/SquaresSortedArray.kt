bff2fed7f118e21fd64310cdc909a7fa09e25c6a
NikhilBajpai <nikhil.cse16@gmail.com>
2021-09-19 11:36:26 +0530

    week 0 day 14

--- app/src/main/java/com/example/dsakotlin/SquaresSortedArray.kt
+++ app/src/main/java/com/example/dsakotlin/SquaresSortedArray.kt
@@ -1,15 +1,31 @@
 package com.example.dsakotlin
 
+import kotlin.math.abs
+
 fun main() {
 
     fun sortedSquares(nums: IntArray): IntArray {
 
-
-
-
-        return nums
+        val numSquare = nums.map { abs(it*it)}
+        println(numSquare)
+        var result = IntArray(nums.size)
+        var left = 0
+        var right = numSquare.size-1
+        var currentPosition = 0
+        while (left <= right){
+            if(numSquare[left] > numSquare[right]){
+                result[currentPosition] = numSquare[left]
+                left++
+            }else{
+                result[currentPosition] = numSquare[right]
+                right--
+            }
+            currentPosition+=1
+        }
+        result.reverse()
+        return result
     }
     val nums = intArrayOf(-7,-3,2,3,11)
-    println(sortedSquares(nums))
+    println(sortedSquares(nums).contentToString())
 
 }
